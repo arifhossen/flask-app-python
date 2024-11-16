feeb5da9e2960eee485b74a6213f6911e98b5f2e
devops-arif+1-at-587726240663 <ariftechinfo@gmail.com>
2024-11-16 09:20:22 +0600

    Yes

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '''Welcome to Cloud Aseem. YouTube destination for complete knowledge,
-            This is Python - Flask APP, We have successfully created a CICD PIPELINE, Thank you so much really happly Flow..'''
+    return ''' This is second'''
 
 @app.route('/health')
 def health():
